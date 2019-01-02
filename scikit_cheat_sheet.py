9cff7744074b1513327524d710a1e8693d23fc94
Laksh <lakshyajeet22@gmail.com>
2019-01-02 20:46:57 +0530

    Add code for logistic regression to cheat sheet

--- scikit_cheat_sheet.py
+++ scikit_cheat_sheet.py
@@ -1,4 +1,3 @@
-
 ##Linear Regression##
 #----------Preparing the data ----------#
 #%%
@@ -20,6 +19,7 @@ lin_reg.predict(X_new)
 #%%
 from sklearn.linear_model import SGDRegressor
 sgd_reg = SGDRegressor(n_iter=50, penalty=None, eta0=0.1)
+#penalty = 'l2' if you want to add ridge regularization
 sgd_reg.fit(X, y.ravel())
 sgd_reg.intercept_, sgd_reg.coef_
 
@@ -84,3 +84,91 @@ from sklearn.linear_model import Ridge
 ridge_reg = Ridge(alpha=1, solver="cholesky")
 ridge_reg.fit(X, y)
 ridge_reg.predict([[1.5]])
+
+##Lasso Regression
+#%%
+from sklearn.linear_model import Lasso
+lasso_reg = Lasso(alpha=0.1)
+lasso_reg.fit(X, y)
+lasso_reg.predict([[1.5]])
+
+##Elastic Net Regression
+##Between lasso and ridge
+##r = 1 is lasso, r = 0 is ridge
+#%%
+from sklearn.linear_model import ElasticNet
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic_net.fit(X, y)
+elastic_net.predict([[1.5]])
+
+#---------- Early Stopping ----------#
+#Stop training as soon as error starts increasing again
+#Error increases due to overfitting
+#%%
+from sklearn.base import clone
+sgd_reg = SGDRegressor(n_iter=1, warm_start=True, penalty=None,
+                        learning_rate="constant", eta0=0.0005)
+minimum_val_error = float("inf")
+best_epoch = None
+best_model = None
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+#%%
+import warnings
+warnings.filterwarnings('ignore')
+for epoch in range(1000):
+    sgd_reg.fit(X_train, y_train)
+    y_val_predict = sgd_reg.predict(X_val)
+    val_error = mean_squared_error(y_val_predict, y_val)
+    if val_error < minimum_val_error:
+        minimum_val_error = val_error
+        best_epoch = epoch
+        best_model = clone(sgd_reg)
+
+
+
+
+
+
+##Logistic Regression##
+#---------- Getting the data ----------#
+#%%
+from sklearn import datasets
+iris = datasets.load_iris()
+list(iris.keys())
+#%%
+X = iris["data"][:,3:] #Petal width
+y = (iris["target"] == 2).astype(np.int) #1 if Iris-Virginica, else 0
+
+#---------- Training the regression model ----------#
+#%%
+from sklearn.linear_model import LogisticRegression
+log_reg = LogisticRegression()
+log_reg.fit(X, y)
+
+#---------- Predicting the petal width probabilities ----------#
+#%%
+X_new = np.linspace(0, 3, 1000).reshape(-1, 1)
+y_proba = log_reg.predict_proba(X_new)
+plt.plot(X_new, y_proba[:, 1], "g-", label="Iris-Virginica")
+plt.plot(X_new, y_proba[:, 0], "b--", label="Not Iris-Virginica")
+
+#%%
+log_reg.predict([[1.7], [1.5]])
+
+#In sklearn, logistic regression is regularized by l2 penalty by default
+#The perameter is C not alpha. The higher C is, the lower is the regularization
+
+
+#---------- Softmax Regression ----------#
+#Scikit uses one-vs-all by default
+#Used if logistic regression for more than 2 classes
+#%%
+X = iris["data"][:, (2, 3)] # petal length, petal width
+y = iris["target"]
+
+softmax_reg = LogisticRegression(multi_class="multinomial",solver="lbfgs", C=10)
+softmax_reg.fit(X, y)
+
+softmax_reg.predict([[5, 2]])
+#%%
+softmax_reg.predict_proba([[5, 2]])
